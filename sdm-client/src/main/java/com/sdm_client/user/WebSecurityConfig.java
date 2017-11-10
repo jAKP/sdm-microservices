a59e74555924399d2ebe77ea569242666f1b0f49
Alok Pandey <pandeyalok.leo@gmail.com>
2017-11-10 21:01:00 +0100

    new

--- sdm-client/src/main/java/com/sdm_client/user/WebSecurityConfig.java
+++ sdm-client/src/main/java/com/sdm_client/user/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.sdm_client.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan("com.sdm_client")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
